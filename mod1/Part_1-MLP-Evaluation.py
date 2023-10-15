b7307f93bde1ff3e17fb2c7035bb3d797536cec8
André Silva <2201724@my.ipleiria.pt>
2023-10-15 21:08:26 +0100

    PART 1&2 DONE (falta respostas)

---
+++ mod1/Part_1-MLP-Evaluation.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+import torch.nn.functional
+
+
+training_set: torch.utils.data.Dataset = torchvision.datasets.MNIST("./data", train=True, download=True, transform=torchvision.transforms.ToTensor())
+validation_set: torch.utils.data.Dataset = torchvision.datasets.MNIST("./data", train=False, download=True, transform=torchvision.transforms.ToTensor())
+
+
+
+from bobnet import BobNet
+from cnn import CNN;
+
+model1 = BobNet()
+model2=CNN()
+
+# batch size
+MLP_BATCH_SIZE=64
+
+# learning rate
+MLP_LEARNING_RATE=0.001
+
+# momentum
+MLP_MOMENTUM=0.9
+
+# training epochs to run
+MLP_EPOCHS=10
+
+# create the training loader
+mlp_training_loader = DataLoader(training_set, batch_size=MLP_BATCH_SIZE, shuffle=True) 
+
+# create the validation loader
+mlp_validation_loader = DataLoader(validation_set, batch_size=MLP_BATCH_SIZE, shuffle=True)
+
+mlp_loss_fn = torch.nn.CrossEntropyLoss()
+
+#mlp_optimizer = torch.optim.SGD(model1.parameters(), lr=MLP_LEARNING_RATE, momentum=MLP_MOMENTUM)
+mlp_optimizer = torch.optim.SGD(model2.parameters(), lr=MLP_LEARNING_RATE, momentum=MLP_MOMENTUM)
+
+import utils
+
+# how many batches between logs
+LOGGING_INTERVAL=100
+
+#utils.train_model(model1, MLP_EPOCHS, mlp_optimizer, mlp_loss_fn, mlp_training_loader, mlp_validation_loader, LOGGING_INTERVAL)
+utils.train_model(model2, MLP_EPOCHS, mlp_optimizer, mlp_loss_fn, mlp_training_loader, mlp_validation_loader, LOGGING_INTERVAL)
--- mod1/cnn.py
+++ mod1/cnn.py
@@ -1,12 +1,25 @@
-from torch import nn, functional as F
+import torch.nn as nn
+import torch.nn.functional as F
 
 class CNN(nn.Module):
-
     def __init__(self):
-
         super().__init__()
-
-        raise NotImplementedError("Define the layers here!")
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 6, 5)  # 1 input channel, 6 output channels, 5x5 kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)  # 6 input channels, 16 output channels, 5x5 kernel
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)  # 16 channels, 4x4 image size
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)  # Output has 10 classes for MNIST
 
     def forward(self, x):
-        raise NotImplementedError("Implement the forward pass here!")
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)  # Max-pooling
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)  # Max-pooling
+        x = x.view(-1, 16 * 4 * 4)  # Flatten for fully connected layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
